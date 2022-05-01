c191d2d5eceaaa0ff0beeb34788ca4caace56ebb
Darealtube <evajavier143@gmail.com>
2022-05-01 11:07:30 +0800

    Change server.ts

--- server.ts
+++ server.ts
@@ -7,44 +7,44 @@ import { schema } from "./graphql/schema";
 import { useServer } from "graphql-ws/lib/use/ws";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 
-const PORT = process.env.PORT || 3000;
-
-dbConnect();
-const app = express();
-const httpServer = createServer(app);
-
-// Create our WebSocket server using the HTTP server we just set up.
-const wsServer = new WebSocketServer({
-  server: httpServer,
-  path: "/graphql",
-});
-
-// Save the returned server's info so we can shutdown this server later
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const serverCleanup = useServer({ schema }, wsServer);
-
-// Set up ApolloServer.
-const server = new ApolloServer({
-  schema,
-  plugins: [
-    // Proper shutdown for the HTTP server.
-    ApolloServerPluginDrainHttpServer({ httpServer }),
-
-    // Proper shutdown for the WebSocket server.
-    {
-      async serverWillStart() {
-        return {
-          async drainServer() {
-            await serverCleanup.dispose();
-          },
-        };
-      },
-    },
-  ],
-  introspection: process.env.NODE_ENV !== "production",
-});
-
 (async () => {
+  const PORT = process.env.PORT || 3000;
+
+  dbConnect();
+  const app = express();
+  const httpServer = createServer(app);
+
+  // Create our WebSocket server using the HTTP server we just set up.
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/graphql",
+  });
+
+  // Save the returned server's info so we can shutdown this server later
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const serverCleanup = useServer({ schema }, wsServer);
+
+  // Set up ApolloServer.
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      // Proper shutdown for the HTTP server.
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+
+      // Proper shutdown for the WebSocket server.
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
+    introspection: process.env.NODE_ENV !== "production",
+  });
+
   await server.start();
 
   // Apply CORS here. app.use(cors()) does not work because 'app' is not the one listening to PORT; It's the HTTP Server.
@@ -63,10 +63,8 @@ const server = new ApolloServer({
     },
   });
 
-  console.log(PORT);
+  // Now that our HTTP server is fully set up, we can listen to it.
+  httpServer.listen(PORT, () => {
+    console.log(`Server is now RUNNING on PORT ${PORT}`);
+  });
 })();
-
-// Now that our HTTP server is fully set up, we can listen to it.
-httpServer.listen(PORT, () => {
-  console.log(`Server is now RUNNING on PORT ${PORT}`);
-});
