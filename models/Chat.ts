73a964f1ebefa0172ac0a7aec349d763762c2ecd
Darealtube <evajavier143@gmail.com>
2022-05-09 12:29:56 +0800

    Adding TTL to Chat

--- models/Chat.ts
+++ models/Chat.ts
@@ -1,14 +1,25 @@
 import { ObjectId } from "mongodb";
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
+import { DateTime } from "luxon";
 
 const ChatSchema = new Schema({
   id: ObjectId,
   anonymous: String,
   confessee: String,
   updatedAt: Number,
-  anonSeen: Boolean,
-  confesseeSeen: Boolean,
+  anonSeen: {
+    type: Boolean,
+    default: false,
+  },
+  confesseeSeen: {
+    type: Boolean,
+    default: false,
+  },
+  expiresIn: {
+    type: Date,
+    default: DateTime.local().plus({ minutes: 15 }),
+  },
 });
 
 export default mongoose.models.Chat || mongoose.model("Chat", ChatSchema);
